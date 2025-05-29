8f7f831278805e66bdb1d9b37c531738ad2af29c
dabu76 <whghtjd45@gmail.com>
2025-05-30 00:18:20 +0900

    Merge conflict resolved in useCurrentLocation

--- src/hooks/useCurrentLocation.jsx
+++ src/hooks/useCurrentLocation.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect } from "react";
 
 export function useCurrentLocation(callback) {
@@ -36,46 +35,3 @@ async function reverseGeocode(lat, lng) {
   );
   return city?.long_name || "";
 }
-=======
-import { useEffect } from "react";
-
-export function useCurrentLocation(setSearch) {
-  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-  console.log(apiKey);
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      async (pos) => {
-        const lat = pos.coords.latitude;
-        const lng = pos.coords.longitude;
-        console.log("位置情報:", lat, lng);
-
-        const cityName = await reverseGeocode(lat, lng);
-        console.log("都市:", cityName);
-        setSearch(cityName);
-      },
-      async () => {
-        // もし位置情報をもらえなかったら名古屋
-        setSearch("名古屋市");
-      }
-    );
-  }, []);
-}
-
-// 逆ジオコーディング:緯度/経度→都市名
-async function reverseGeocode(lat, lng) {
-  const response = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&language=ja&key=${
-      import.meta.env.VITE_GOOGLE_MAPS_API_KEY
-    }`
-  );
-
-  const data = await response.json();
-  const result = data.results?.[0];
-  if (!result) return "";
-
-  const city = result.address_components.find((c) =>
-    c.types.includes("locality")
-  );
-  return city?.long_name || "";
-}
->>>>>>> main
