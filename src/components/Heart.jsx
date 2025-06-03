86c080831d4025544f2d8706fcae4aaab5c021b2
dabu76 <whghtjd45@gmail.com>
2025-06-03 21:13:49 +0900

    ハートボタンに「気に入り」機能を追加

--- src/components/Heart.jsx
+++ src/components/Heart.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 
-export function HeartButton() {
+export function HeartButton({ caregiverId, user }) {
   const [liked, setLiked] = useState(false);
   const [count, setCount] = useState(0);
-
   const toggleLike = () => {
     setCount(liked ? count - 1 : count + 1);
     setLiked(!liked);
+    if (user.favorite.includes(caregiverId)) {
+      user.favorite = user.favorite.filter((num) => num !== caregiverId);
+      console.log(user.favorite);
+    } else {
+      user.favorite.push(caregiverId);
+      console.log(user.favorite);
+    }
   };
 
   return (
